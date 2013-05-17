579ae2ac63b0ebbeb423490f9f595118be5bc204
Satoshi Takano <satoxi.tacano@gmail.com>
2013-05-17 16:14:39 +0900

    choose images for uploading stepwise

---
+++ app/assets/javascripts/form.js
@@ -0,0 +1,25 @@
+var $image_fields = $("#image_selector");
+$($image_fields.find("input")[0]).bind("change", chosen);
+
+function chosen(e) {
+    $(e.currentTarget).unbind("change", chosen);
+    
+    var $selectors = $image_fields.find("div");
+    var selector = $selectors[0];
+    
+    var $clone = $(selector).clone();
+
+    var input = $clone.find("input");
+    var name = input.attr("name");
+    var id = input.attr("id");
+
+    var idx = $selectors.length;
+    name = name.replace(/image_[0-9]/, "image_" + idx);
+    id = id.replace(/image_[0-9]/, "image_" + idx);
+    input.attr("name", name);
+    input.attr("id", id);
+
+    $clone.find("label").text("Image " + idx);
+    $clone.find("input").bind("change", chosen);
+    $image_fields.append($clone);
+}
