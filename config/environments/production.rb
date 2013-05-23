52cd609589bca193c2192d7ed3325fd41fe6042f
Satoshi Takano <satoxi.tacano@gmail.com>
2013-05-23 18:23:04 +0900

    fix configuration for the asset pipeline

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,6 +19,8 @@ Hatarakiari::Application.configure do
 
   # Generate digests for assets URLs
   config.assets.digest = true
+  
+  config.assets.precompile += %w(*.css)
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
