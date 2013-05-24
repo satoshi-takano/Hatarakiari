a4168de7e1d634459c972f9bea6e3c648f46ae63
Satoshi Takano <satoxi.tacano@gmail.com>
2013-05-24 18:11:59 +0900

    add form validations

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -4,5 +4,7 @@ class Guest < ActiveRecord::Base
   attr_accessible :login_id, :login_password, :user_id, :memo
   validates_presence_of :login_id, :login_password, :user_id
   validates_numericality_of :user_id
+  validates_length_of :login_id, :in => 1..32
+  validates_length_of :login_password, :in => 6..32
   validates :memo, :length => { :maximum => 140}
 end
