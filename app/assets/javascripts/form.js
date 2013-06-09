ad09670c42a0e3793a5a827a8b548a3927637354
Satoshi Takano <satoxi.tacano@gmail.com>
2013-06-09 23:35:46 +0900

    show thumbnails if the work already has images

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -1,28 +1,30 @@
 var $image_fields = $("#image_selector");
-$($image_fields.find("input")[0]).bind("change", chosen)[0]._index = 0;
+var inputs = $image_fields.find("input");
+$(inputs[inputs.length - 1]).bind("change", chosen)[0]._index = 0;
 
 
 function chosen(e) {
-    if (e.currentTarget._index == 4) return;
+    if ($image_fields.find("input").length == 5) return;
     
     $(e.currentTarget).unbind("change", chosen);
     
-    var $selectors = $image_fields.find("div");
+    var $selectors = $image_fields.find(".cloneable");
     var selector = $selectors[0];
     
     var $clone = $(selector).clone();
+    $clone.removeClass("has_image");
 
     var input = $clone.find("input");
     var name = input.attr("name");
     var id = input.attr("id");
 
-    var idx = $selectors.length;
+    var idx = $image_fields.find("input").length;
     name = name.replace(/image_[0-9]/, "image_" + idx);
     id = id.replace(/image_[0-9]/, "image_" + idx);
     input.attr("name", name);
     input.attr("id", id);
 
-    $clone.find("label").text("Image " + idx);
+    $clone.find("h3").text(idx + 1);
     $clone.find("input").bind("change", chosen)[0]._index = idx;
     $image_fields.append($clone);
 }
